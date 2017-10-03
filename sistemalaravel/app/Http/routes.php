e7dee92e5f18f27ce5c3b4a546072a1a9ca5c3f3
andrei7v <30575028+andrei7v@users.noreply.github.com>
2017-10-03 13:35:46 -0500

    leccion3
    agregar plantilla

--- sistemalaravel/app/Http/routes.php
+++ sistemalaravel/app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
